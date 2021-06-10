f5589998a786a716943ad860361852d4d027ca10
Justin Tabinski <justin_1791@hotmail.com>
2021-06-10 12:07:38 -0500

    redo last commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// stops api link // 20210610112913
+// https://api.winnipegtransit.com/v3/stops/10064.json?api-key=BW4rOzRErQbKDPpVzhq6&street=worthington
+// example of data  "stop": {
+    // "key": 10064,
+    // "name": "Northbound Osborne at Glasgow",
+    // "number": 10064,
+    // "direction": "Northbound",
+    // "side": "Nearside",
+    // "street": {
+    //   "key": 2715,
+    //   "name": "Osborne Street",
+    //   "type": "Street"
+    // },
+// my api key BW4rOzRErQbKDPpVzhq6
+// streets api link // 20210610113044
+// https://api.winnipegtransit.com/v3/streets/2715.json?api-key=BW4rOzRErQbKDPpVzhq6&name=osborne
+// example of data "street": {
+  //   "key": 2715,
+  //   "name": "Osborne Street",
+  //   "type": "Street"
+  // },
+  // "query-time": "2021-06-10T11:30:43"
